fa0b134417065edf7c4017272fcbf6da9e8cc583
diskobox <elmar.nubbemeyer@gmail.com>
2015-06-20 16:11:35 +0200

    Finished assignment 2 of R Programming
    Wrote makeCacheMatrix and cacheSolve functions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Coursera R Programming Assignment 2
+# Defines functions makeCacheMatrix and cacheSolve to define a matrix and
+# cache its inverse.
 
-## Write a short comment describing this function
+## makeChacheMatrix
+# For a given matrix, creates an object containing functions to set and 
+# reference the original matrix and its inverse.
+# Arguments:
+# x = invertible matrix
+# Returns:
+# A list of functions get, set, setinverse and getinverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    set <- function(y) {
+      x <<- y
+      m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) i <<- inverse
+    getinverse <- function() i
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve
+# For a given makeCacheMatrix object, returns the inverse of the matrix.
+# Arguments:
+# x = makeCacheMatrix object 
+# Returns:
+# Inverse of the matrix in x
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
+
+mtrx <- matrix(rnorm(25), 5, 5)
+a <- makeCacheMatrix(mtrx)
+a_inv <- cacheSolve(a)
+a$get()
+solve(a_inv)
+
